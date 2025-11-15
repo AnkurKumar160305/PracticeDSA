3a1063805811f9e5b862c7cd0bbddba9b22e271c
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2025-11-15 23:41:11 +0530

    new files added

---
+++ Practice dsa/1-TwoSum.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int,int> mp;
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+        int first=target-nums[i];
+        if(mp.find(first)!=mp.end()){
+            return {mp[first],i};
+        }
+        mp[nums[i]]=i;
+    }
+
+    return {0,0};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = twoSum(nums, target);
+    cout << result[0] << " " << result[1] << endl;
+}
