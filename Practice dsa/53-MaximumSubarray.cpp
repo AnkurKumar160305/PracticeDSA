fd7945de3d7988e479697169c96688ca80460155
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2025-11-22 01:10:41 +0530

    addded more files

---
+++ Practice dsa/53-MaximumSubarray.cpp
@@ -0,0 +1,16 @@
+	
+// Kadane's Algorithm, maximum subarray sum
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int maxEnd=nums[0];
+        int maxSoFar=nums[0];
+        for(int i=1;i<n;i++){
+            maxEnd=max(nums[i],maxEnd+nums[i]);
+            maxSoFar = max(maxSoFar, maxEnd);
+        }
+        return maxSoFar;
+        
+    }
+};
