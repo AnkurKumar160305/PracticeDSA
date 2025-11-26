0e72c42cce38c25322cfb4cf4eacf63beb36d7b1
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2025-11-26 22:07:14 +0530

    added new matrix files

---
+++ Practice dsa/73-SetMatrixZero.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+
+        bool firstRow=false;
+        bool firstCol=false;
+        for(int i=0;i<col;i++){
+            if(matrix[0][i]==0){
+                firstRow=true;
+            }
+        }
+
+        for(int i=0;i<row;i++){
+            if(matrix[i][0]==0){
+                firstCol=true;
+            }
+        }
+
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+        if(firstRow){
+            for(int i=0;i<col;i++){
+                matrix[0][i]=0;
+            }
+        }
+
+        if(firstCol){
+            for(int i=0;i<row;i++){
+                matrix[i][0]=0;
+            }
+        }
+    }
+};
