74017a070f422ad84d7f0445f97d0d98aacf76be
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2025-12-09 12:27:04 +0530

    added new  file

---
+++ Practice dsa/410-SplitarrayLargestSum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    int countSplit(vector<int>& nums,int mid){
+        int first=0;
+        int count=1;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(first+nums[i]>mid){
+                count++;
+                first=nums[i];
+            }else{
+                first+=nums[i];
+            }
+        }
+        return count;
+    }
+public:
+    int splitArray(vector<int>& nums, int k) {
+        unordered_map<int,int> m;
+        int n=nums.size();
+        int left=*max_element(nums.begin(),nums.end());
+        int right=accumulate(nums.begin(),nums.end(),0);
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(countSplit(nums,mid)<=k){
+                right=mid;
+            }else{
+                left=mid+1;
+            }
+        }
+        return left;
+    }
+};
