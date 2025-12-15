bff468173e6b5cda4bb24a00cd938e26464c2bc5
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2025-12-15 16:06:14 +0530

    added new file

--- Practice dsa/Strings/451-SortCharByFrequency.cpp
+++ Practice dsa/Strings/451-SortCharByFrequency.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        
+        unordered_map<char, int> freq;
+        for (char c : s) {
+            freq[c]++;
+        }
+
+        priority_queue<pair<int,char>> pq;
+        for(auto i:freq){
+            pq.push({i.second,i.first});
+        }
+        string str="";
+        while(!pq.empty()){
+            auto top=pq.top();
+            pq.pop();
+            int count=top.first;
+            char ch=top.second;
+            while(count){
+                str+=ch;
+                count--;
+            }
+        }
+        return str;
     }
 };
