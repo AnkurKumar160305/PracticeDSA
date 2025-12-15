4d627980e80d425a455df16792d97d8deed8f210
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2025-12-15 22:29:08 +0530

    added new file

---
+++ Practice dsa/Strings/13-RomanToInteger.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int> m={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        int sum=0;
+        int prev=0;
+        for(int i=s.size()-1;i>=0;i--){
+            int curr=m[s[i]];
+            if(curr<prev){
+                sum-=m[s[i]];
+            }else{
+                sum+=m[s[i]];
+            }
+            prev=curr;
+        }
+        return sum;
+    }
+};
+
+//Optimal
+
+class Solution {
+public:
+    int value(char c) {
+        if (c == 'I') return 1;
+        else if (c == 'V') return 5;
+        else if (c == 'X') return 10;
+        else if (c == 'L') return 50;
+        else if (c == 'C') return 100;
+        else if (c == 'D') return 500;
+        else if (c == 'M') return 1000;
+        return 0;
+    }
+
+    int romanToInt(string s) {
+        int ans = 0;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+            if (i + 1 < n && value(s[i]) < value(s[i + 1])) {
+                ans -= value(s[i]);
+            } else {
+                ans += value(s[i]);
+            }
+        }
+        return ans;
+    }
+};
---
+++ Practice dsa/Strings/8-StringToInteger(atoi).cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i=0,n=s.size();
+        int sign=1;
+        long long ans=0;
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
+
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') sign = -1;
+            i++;
+        }
+
+        while(i<n && s[i]>='0' && s[i]<='9'){
+            ans=ans*10+(s[i]-'0');
+            if (sign == 1 && ans > INT_MAX) return INT_MAX;
+            if (sign == -1 && -ans < INT_MIN) return INT_MIN;
+            i++;
+        }
+
+
+        return sign*ans;
+    }
+};
