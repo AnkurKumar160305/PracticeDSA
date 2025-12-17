f851f4b46c18019b636a67a32633a6a6c08a8d08
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2025-12-17 16:10:25 +0530

    added new  file

--- Practice dsa/LinkedList/Introduction.cpp
+++ Practice dsa/LinkedList/Introduction.cpp
@@ -11,6 +11,7 @@ class Node{
     }
 };
 
+//Step1-Printing Linked List
 void printingLL(Node* head){
     Node* temp=head;
     while(temp!=NULL){
@@ -19,11 +20,60 @@ void printingLL(Node* head){
     }
 }
 
+//Step2-Inserting form Beginning
+void insertatHead(Node* &head,int val){
+    Node* newnode=new Node(val);
+    newnode->next=head;
+    head=newnode;
+}
+
+void insertatEnd(Node* &head,int val){
+    Node* newnode=new Node(val);
+    if(head==NULL){
+        head=newnode;
+        return;
+    }
+    Node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=newnode;
+}
+
+//Insert at given Position(1 based index)
+void insertatPosition(Node* &head,int pos,int val){
+    Node* newnode=new Node(val);
+    if(pos==1){
+        insertatHead(head,val);
+        return;
+    }
+    int cnt=1;
+    Node* temp=head;
+    while(cnt<pos-1 && temp!=NULL){
+        temp=temp->next;
+        cnt++;
+    }
+
+    if (temp == NULL) return;
+    newnode->next=temp->next;
+    temp->next=newnode;
+
+
+}
+
 int main(){
     Node* head=new Node(1);
     Node* first=new Node(2);
     Node* second=new Node(3);
     head->next=first;
     first-> next=second;
+    printingLL(head);
+    cout<<endl;
+
+    insertatHead(head,0);
+    printingLL(head);
+    cout<<endl;
+    
+    insertatEnd(head,4);
     printingLL(head);
 }
