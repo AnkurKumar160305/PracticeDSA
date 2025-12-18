71170fde7bde6b41b52517470150e314d6177fc4
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2025-12-18 15:05:41 +0530

    Added new file

--- Practice dsa/LinkedList/DoublyLL.cpp
+++ Practice dsa/LinkedList/DoublyLL.cpp
@@ -62,6 +62,31 @@ void insertatEnd(Node* &head,int val){
     temp->next=newnode;
     newnode->prev=temp;
 }
+
+void insertatPosition(Node* &head,int pos,int val){
+    if(pos==1){
+        insertatBeginning(head,val);
+        return;
+    }
+    int cnt=0;
+    Node* temp=head;
+    while(cnt<pos-1 && temp!=NULL){
+        temp=temp->next;
+    }
+    if(temp==NULL) return;
+
+    Node* newnode=new Node(val);
+    newnode->next=temp->next;
+    newnode->prev=temp;
+
+    if(temp->next!=NULL){
+        temp->next->prev=newnode;
+    }
+
+    temp->next=newnode;
+}
+
+
 int main(){
     Node* head=new Node(0);
 }
