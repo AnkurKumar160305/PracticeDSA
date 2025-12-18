b9c6f6b78e4de7974defee28f66d8fd043e53036
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2025-12-18 14:37:03 +0530

    Added new file

---
+++ Practice dsa/LinkedList/DoublyLL.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+    public:
+    int data;
+    Node* next;  
+    Node* prev;
+    Node(int d){
+        data=d;
+        next=NULL;
+        prev=NULL;
+    }  
+}
+
+void printforwardLL(Node* head){
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+void printbackwardLL(Node* head){
+    if(head==NULL) return;
+
+    Node* temp = head;
+    while (temp != NULL) {
+        temp = temp->next;
+    }
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->prev;
+    }
+}
+
+
+void insertatBeginning(Node* &head,int val){
+    Node* newnode=new Node(val);
+    if(head==NULL){
+        head=newnode;
+        return;
+    }
+    newnode->next=head;
+    head->prev=newnode;
+    head=newnode;
+}
+int main(){
+    Node* head=new Node(0);
+}
