ad3763dfab63b281415802cffd642815ad6f788a
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2026-01-08 20:07:55 +0530

    added new file

--- Practice dsa/bubblesort.cpp
+++ Practice dsa/bubblesort.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 void BubbleSort(int arr[],int n){
-    for(int i=n-1;i>=0;i--){;
+    for(int i=0;i<n-1;i++){;
         int flag=0;
-        for(int j=0;j<=i-1;j++){
+        for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
                 flag=1;
