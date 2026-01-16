3f8717f69f98c384c072b8d2f70af3b0170c8ebc
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2026-01-16 22:10:44 +0530

    added new file

--- Practice dsa/Recursion/139-WordBreak.cpp
+++ Practice dsa/Recursion/139-WordBreak.cpp
@@ -1,1 +1,27 @@
-ankur kumar  jknjccgmg
+4223
+11
+2
+
+int countDigits(int n){
+    if(n==0){
+        return 0;
+    }
+    int cnt=0;
+    while(n!=0){
+        n=n/10;
+        cnt++;
+    }
+    return cnt;
+}
+int singleDigit(int n){
+    if(countDigits(n)==1){
+        return n;
+    }
+    int sum=0;
+    while(n!=0){
+        int d=n%10;
+        n=n/10;
+        sum+=d;
+    }
+    return singleDigit(sum);
+}
