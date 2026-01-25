4689929db0d2d6515baf8790722a0791ffb09b5c
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2026-01-25 15:02:25 +0530

    added new file

--- Practice dsa/AllocateMinNoofPages.cpp
+++ Practice dsa/AllocateMinNoofPages.cpp
@@ -1,45 +1,40 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-
-int countStudents(vector<int>& arr,int mid){
-    int count=1;
-    int pages=0;
-    for(int i=0;i<arr.size();i++){
-        if(pages+arr[i]<=mid){
-            pages+=arr[i];
-        }else{
-            count++;
-            pages=arr[i];
+class Solution {
+    int countStudents(vector<int>& arr,int mid,int k){
+        int st=1;
+        int pages=0;
+        
+        for(int i=0;i<arr.size();i++){
+            if(pages+arr[i]<=mid){
+                pages+=arr[i];
+            }else{
+                st++;
+                pages=arr[i];
+            }
         }
+        return st;
     }
-    return count;
-}
-
-int findPages(vector<int>& arr,int n,int m){
-    if (m > n) return -1;
-
-    int left=*max_element(arr.begin(),arr.end());
-    int right=accumulate(arr.begin(),arr.end(),0);
-
-    while(left<=right){
-        int mid=left+(right-left)/2;
-        int students=countStudents(arr,mid);
-        if(students>m){
-            left=mid+1;
-        }else{
-            right=mid-1;
+  public:
+    int findPages(vector<int> &arr, int k) {
+        int n=arr.size();
+        if(k>n){
+            return -1;
         }
+        int left=*max_element(arr.begin(),arr.end());
+        int right=accumulate(arr.begin(),arr.end(),0);
+        int ans=-1;
+        
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            int st=countStudents(arr,mid,k);
+            
+            if(st<=k){
+                ans=mid;
+                right=mid-1;
+            }else{
+                left=mid+1;
+                
+            }
+        }
+        return ans;
     }
-    return left;
-}
-
-int main()
-{
-    vector<int> arr = {25, 46, 28, 49, 24};
-    int n = 5;
-    int m = 4;
-    int ans = findPages(arr, n, m);
-    cout << "The answer is: " << ans << "\n";
-    return 0;
-}
+};
