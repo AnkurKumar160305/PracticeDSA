d30d6447e8e0f439dbe49d920c4c73d8d9efaae4
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2026-02-02 14:38:48 +0530

    added new file

---
+++ Practice dsa/SlidingWindow/FirstNegativeinEveryWindowofSizeK.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    vector<int> firstNegInt(vector<int>& arr, int k) {
+        // write code here
+        int n=arr.size();
+        queue<int> neg;
+        vector<int> ans;
+        
+        int i=0,j=0;
+        while(j<n){
+            if(arr[j]<0){
+                neg.push(arr[j]);
+            }
+            if(j-i+1<k){
+                j++;
+            }else if(j-i+1==k){
+                if(!neg.empty()){
+                    int negative=neg.front();
+                    ans.push_back(negative);
+                    if(neg.front()==arr[i]){
+                        neg.pop();
+                    }
+                }else{
+                    ans.push_back(0);
+                }
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
