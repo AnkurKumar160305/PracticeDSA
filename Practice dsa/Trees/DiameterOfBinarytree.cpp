78fd9d4c9b84b555dcc06eec64a82e19f9440f03
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2026-06-20 01:13:18 +0530

    diameter of BT

--- Practice dsa/Trees/DiameterOfBinarytree.cpp
+++ Practice dsa/Trees/DiameterOfBinarytree.cpp
@@ -1,30 +1,40 @@
-class Solution
-{
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
     int height(Node *root)
     {
-        // code here
         if (root == NULL)
         {
-            return -1;
+            return 0;
         }
         int left = height(root->left);
         int right = height(root->right);
         int ans = max(left, right) + 1;
         return ans;
     }
-
-public:
-    int diameter(Node *root)
-    {
+  public:
+    int diameter(Node* root) {
         // code here
-        if (root == NULL)
-        {
+        if(root==NULL){
             return 0;
         }
-        int op1 = diameter(root->left);
-        int op2 = diameter(root->right);
-        int op3 = height(root->left) + height(root->right) + 2;
-        int ans = max(op1, max(op2, op3));
+        int left=diameter(root->left);
+        int right=diameter(root->right);
+        int mix=height(root->left)+height(root->right);
+        int ans=max(left,max(right,mix));
         return ans;
     }
 };
@@ -42,23 +52,24 @@ public:
  * };
  */
 class Solution {
-    pair<int,int> diameterFast(TreeNode* root){
+    pair<int,int> diameterFast(Node* root){
         if(root==NULL){
             return {0,0};
         }
-        auto left=diameterFast(root->left);
-        auto right=diameterFast(root->right);
-        int h=max(left.second,right.second)+1;
-
+        pair<int,int> left=diameterFast(root->left);
+        pair<int,int> right=diameterFast(root->right);
+        
         int op1=left.first;
         int op2=right.first;
         int op3=left.second+right.second;
-
-        int dia=max(op1,max(op2,op3));
-        return {dia,h};
+        
+        pair<int,int> ans;
+        ans.first=max(op1,max(op2,op3));
+        ans.second=max(left.second,right.second)+1;
+        return ans;
     }
-public:
-    int diameterOfBinaryTree(TreeNode* root) {
+  public:
+    int diameter(Node* root) {
         return diameterFast(root).first;
     }
 };
