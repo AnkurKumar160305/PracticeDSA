f740ce611e8ceb564921597ea57f1ee1dad2d72d
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2026-06-28 15:39:58 +0530

    solved trees question

--- Practice dsa/Trees/222-countNodes.cpp
+++ Practice dsa/Trees/222-countNodes.cpp
@@ -1,3 +1,43 @@
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        int cnt=0;
+        while(!q.empty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode* temp=q.front();
+                cnt++;
+                q.pop();
+                if(temp->left){
+                    q.push(temp->left);
+                }
+                if(temp->right){
+                    q.push(temp->right);
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -18,5 +58,52 @@ public:
         int l=countNodes(root->left);
         int r=countNodes(root->right);
         return l+r+1;
+    }
+};
+
+
+//optimal
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int findLeftHeight(TreeNode* node){
+        int height=0;
+        while(node){
+            height++;
+            node=node->left;
+        }
+        return height;
+    }
+    int findRightHeight(TreeNode* node){
+        int height=0;
+        while(node){
+            height++;
+            node=node->right;
+        }
+        return height;
+    }
+
+public:
+    int countNodes(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        int lh=findLeftHeight(root);
+        int rh=findRightHeight(root);
+
+        if(lh==rh){
+            return (1<<lh)-1;
+            //2^(h-1)
+        }
+        return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
