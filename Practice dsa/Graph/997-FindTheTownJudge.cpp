1696c42f884b63d3840454c108307e6549a7d3b4
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2026-07-09 14:11:05 +0530

    find town judge

--- Practice dsa/Graph/997-FindTheTownJudge.cpp
+++ Practice dsa/Graph/997-FindTheTownJudge.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> indegree(n + 1, 0);
-        vector<int> outdegree(n + 1, 0);
 
-        for(auto &it:trust){
-            outdegree[it[0]]++;
-            indegree[it[1]]++;
+        vector<int> score(n + 1, 0);
+
+        for (auto &it : trust) {
+            score[it[0]]--;
+            score[it[1]]++;
         }
 
-        for(int i=1;i<=n;i++){
-            if(outdegree[i]==0 && indegree[i]==n-1){
+        for (int i = 1; i <= n; i++) {
+            if (score[i] == n - 1)
                 return i;
-            }
         }
 
         return -1;
-
-
     }
 };
