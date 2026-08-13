f0b618c2d436ad69dc076defe1481197e51e471d
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2026-08-13 20:48:37 +0530

    fibbonacci practice

--- Practice dsa/DP/Fibbonacci.cpp
+++ Practice dsa/DP/Fibbonacci.cpp
@@ -1,17 +1,39 @@
+//Fibbonacci Number
 #include<bits/stdc++.h>
 using namespace std;
 
-int fibb(int n,vector<int>& dp){
-        if(n<=1){
-                return n;
-        }
-        if(dp[n]!=-1) return dp[n];
+//Top Down Approach
+//Memoization+Recursion
 
-        dp[n]=fibb(n-1,dp)+fibb(n-2,dp);
+// int fibb(int n,vector<int>& dp){
+//         if(n<=1){
+//                 return n;
+//         }
+//         if(dp[n]!=-1) return dp[n];
 
-        return dp[n];
-}
+//         dp[n]=fibb(n-1,dp)+fibb(n-2,dp);
 
+//         return dp[n];
+// }
+// int main()
+// {
+//         /*
+//         *  Write your code here. 
+//          *  Read input as specified in the question.
+//          *  Print output as specified in the question.
+//         */
+//         int n;
+//         cin>>n;
+//         vector<int> dp(n+1,-1);
+
+//         cout<<fibb(n,dp)<<endl;
+
+//         return 0;
+// }
+
+//OR
+//Tabulation Approach
+//Bottom Up Approach
 int main()
 {
         /*
@@ -23,7 +45,40 @@ int main()
         cin>>n;
         vector<int> dp(n+1,-1);
 
-        cout<<fibb(n,dp)<<endl;
+        dp[0]=0;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        cout<<dp[n]<<endl;
+
+        return 0;
+}
+
+
+//Space optimization (Optimal Approach)
+int main()
+{
+        /*
+        *  Write your code here. 
+         *  Read input as specified in the question.
+         *  Print output as specified in the question.
+        */
+        int n;
+        cin>>n;
+
+        int prev2=0;
+        int prev1=1;
+
+        for(int i=2;i<=n;i++){
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+
+        cout<<prev1<<endl;
 
         return 0;
 }
